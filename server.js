18f6ecaca711feb742ff7156d44ee7d06b6f9dc2
leoloveewy <hs1718@nyu.edu>
2015-02-04 23:33:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ var app = new servi(true);
 
 port(3002);
 serveFiles("public");  
-route('/',show);
+route('indext',show);
 route('/Photos',show2);
 route('/Music',show3);
 function show(request){
 	request.respond('This is homework 1');
 }
 function show2(request){
-	console.log("This is my photos");
-	request.serveFile("photos");
+	request.respond('This is my photos');
+	request.serveFile("photos.html");
 }
 function show3(request){
 	 request.serveFile('music.html');
 
 }
-request.serveFile('index.html');
+
 start();
