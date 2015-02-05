f1a0e544ec6ffc24bc020c93618bb63b3c4e398e
leoloveewy <hs1718@nyu.edu>
2015-02-04 23:34:29 -0500

    Update server2.js

--- server2.js
+++ server2.js
@@ -3,7 +3,7 @@ var app= new servi(true);
 port(4000);
 serveFiles("public"); 
 route('/myPage/:myPageName',showPageName);
-function showPageName(){
+function showPageName(request){
 	request.respont("now you are at:"+request.params.myPageName);
 
 }
